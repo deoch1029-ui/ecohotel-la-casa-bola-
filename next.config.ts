6c5429f47bcf846def133a39c28f3f02baceff36
Z User <z@container>
2026-04-30 21:12:24 +0000

    7ad07b10-bdc7-4ffc-bd11-6fd0ad5981a8

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: false,
   trailingSlash: true,
+  allowedDevOrigins: [
+    "preview-chat-f2fc75e3-9373-4cb7-a105-db2469b95143.space-z.ai",
+    ".space-z.ai",
+    ".space.chatglm.site",
+  ],
 };
 
 export default nextConfig;
